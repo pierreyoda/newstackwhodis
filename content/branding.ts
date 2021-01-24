ecf6f0976a35cf0d4d6938d577cd46035dfc2187
pierreyoda <pierreyoda@users.noreply.github.com>
2021-01-24 15:49:54 +0100

    :fire: Remove all extra boilerplate

---
+++ content/branding.ts
@@ -0,0 +1,10 @@
+export const name = "Prava";
+
+export const copyrightNotice = "Copyright © 2020 Prava";
+
+const Branding = {
+  name,
+  copyrightNotice,
+};
+
+export default Branding;
---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class PravaDocument extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const initialProps = await Document.getInitialProps(context);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+};
+
+export default PravaDocument;
