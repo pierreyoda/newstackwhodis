ac950f08fc3c274bdfeddb941a1e5d758fe809e3
pierreyoda <pierreyoda@users.noreply.github.com>
2021-03-24 20:04:10 +0100

    :sparkles: Add emoji support for GitHub projects' name and description

--- src/api/static.ts
+++ src/api/static.ts
@@ -1,4 +1,16 @@
+import { emojify } from "node-emoji";
+
 import { RepositoriesData } from "@/api/fetcher";
 import GithubPublicRepositoriesMeta from "~/projects/public_repositories.json";
 
-export const parsedRepositoriesData = (): RepositoriesData => GithubPublicRepositoriesMeta;
+export const parsedRepositoriesData = (): RepositoriesData => {
+  const { metadata, githubRepositories }: RepositoriesData = GithubPublicRepositoriesMeta;
+  return {
+    metadata,
+    githubRepositories: githubRepositories.map(({ name, description, ...meta }) => ({
+      name: emojify(name),
+      description: description ? emojify(description) : null,
+      ...meta,
+    })),
+  };
+};
