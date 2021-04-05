60b3589671cdadb650029dab4dc41908eedb4f7b
pierreyoda <pierreyoda@users.noreply.github.com>
2021-04-05 17:59:31 +0200

    :bug: Fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,21 @@ import { useContext } from "react";
 import { GetStaticProps, NextPage } from "next";
 
 import { getBlogPostsSlugs } from "@/api/posts";
-import { RepositoriesData } from "@/api/fetcher";
 import { parsedRepositoriesData } from "@/api/static";
 import { MainLayoutContext } from "@/layouts/MainLayout";
 import ContentPanel, { GithubProjectMeta } from "@/components/layout/ContentPanel";
 
 interface HomePageProps {
-  repositoriesData: RepositoriesData;
+  githubProjects: readonly GithubProjectMeta[];
 }
 
-const HomePage: NextPage<HomePageProps> = ({ repositoriesData: { githubRepositories } }) => {
+const HomePage: NextPage<HomePageProps> = ({ githubProjects }) => {
   const { selectedCategory } = useContext(MainLayoutContext);
 
   return (
     <div className="flex flex-col px-2 md:px-8">
       <h1 className="mb-4 text-4xl font-bold md:mb-12">Personal Projects</h1>
-      <ContentPanel githubProjects={githubRepositories} highlightContentCategory={selectedCategory} />
+      <ContentPanel githubProjects={githubProjects} highlightContentCategory={selectedCategory} />
     </div>
   );
 };
@@ -27,16 +26,13 @@ export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
   const repositoriesData = await parsedRepositoriesData();
   return {
     props: {
-      repositoriesData: {
-        ...repositoriesData,
-        githubRepositories: repositoriesData.githubRepositories.map(
-          ({ name, ...meta }): GithubProjectMeta => ({
-            name,
-            ...meta,
-            blogPostSlug: postsSlugs.find(slug => slug === `project-${name}`) ?? null,
-          }),
-        ),
-      },
+      githubProjects: repositoriesData.githubRepositories.map(
+        ({ name, ...meta }): GithubProjectMeta => ({
+          name,
+          ...meta,
+          blogPostSlug: postsSlugs.find(slug => slug === `project-${name}`) ?? null,
+        }),
+      ),
     },
   };
 };
