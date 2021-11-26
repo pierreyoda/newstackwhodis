685f712a7787d7f728f35b8758567a2333db1a1f
pierreyoda <pierreyoda@users.noreply.github.com>
2021-11-26 18:53:30 +0100

    :fire: Remove some unused files

--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export type PartiallyRequired<T extends Record<string, any>, K extends keyof T> = T & {
-  [P in K]-?: Exclude<T[], null>;
-};
-
-export type Promised<T> = T extends PromiseLike<infer U> ? U : T;
