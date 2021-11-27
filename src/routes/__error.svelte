fa0ac4d65c3b6f42c3c2143effdb722281223d46
pierreyoda <pierreyoda@users.noreply.github.com>
2021-11-27 14:55:49 +0100

    :sparkles: Basic error (most notably 404s) page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,25 @@
+<script context="module" lang="ts">
+  import type { ErrorLoad } from "@sveltejs/kit";
+
+  export const load: ErrorLoad = ({ error, status }) => ({
+    props: {
+      message: `${status}: ${error.message}`,
+    },
+  });
+</script>
+
+<script lang="ts">
+  export let message: string;
+</script>
+
+<div class="container">
+  <h2>Error</h2>
+  <p class="message">{message ?? 'An error has occurred.'}</p>
+</div>
+
+<style lang="postcss">
+  .container {
+    @apply flex flex-col items-start justify-start w-full h-full pl-10;
+    @apply text-gray-lighter;
+  }
+</style>
