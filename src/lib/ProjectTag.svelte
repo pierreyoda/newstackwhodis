ea070f5a54b80d9665f05400852d3e610536e6bc
pierreyoda <pierreyoda@users.noreply.github.com>
2022-08-06 20:15:39 +0200

    :lipstick: Homepage: remove background colors from project tags

--- src/lib/ProjectTag.svelte
+++ src/lib/ProjectTag.svelte
@@ -1,30 +1,18 @@
 <script lang="ts">
   import type { ProjectTagType } from "../api/github/project-tags";
 
-  const backgroundColorByTag: Record<ProjectTagType, string> = {
-    "C#": "#aaffaa",
-    "C++": "#ffffff",
-    Java: "#ffaaaa",
-    Rust: "#d62626",
-    React: "#61dafb",
-    "Vue.js": "#42b983",
-    Unity3D: "#ccffcc",
-    Typescript: "#3178c6",
-  };
-
   export let tag: ProjectTagType;
   export let extraClass: string | undefined;
 
-  $: tagColor = backgroundColorByTag[tag];
   $: tagClass = `project-tag ${extraClass ?? ""}`;
 </script>
 
-<div class={tagClass} style="background-color: {tagColor}">
+<div class={tagClass}>
   {tag}
 </div>
 
 <style lang="postcss">
   .project-tag {
-    @apply px-2 py-1 rounded-md font-medium text-xs;
+    @apply py-1 rounded-md font-bold text-xs text-gray-lighter mr-2;
   }
 </style>
