b2ea73c22f37e5ba1949ef05eba31233b4848f50
pierreyoda <pierreyoda@users.noreply.github.com>
2022-09-27 20:14:48 +0200

    :fire: Remove unnecessary files

--- src/content/authors.ts
+++
@@ -1,4 +0,0 @@
-export const pierreYvesDiallo = {
-  name: "Pierre-Yves Diallo",
-  email: "pierreyoda@users.noreply.github.com",
-};
--- src/layouts/types.ts
+++
@@ -1,8 +0,0 @@
-import { NextPage } from "next";
-
-/**
- * Allows to use the `getLayout` per-page pattern on a layout basis to avoid repetition.
- *
- * @see https://adamwathan.me/2019/10/17/persistent-layout-patterns-in-nextjs/
- */
-export type LayoutGetter = (page: NextPage) => JSX.Element;
