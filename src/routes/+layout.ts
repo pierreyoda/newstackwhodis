f506875a3e60232cca6c8a92b4ad3f6f050d40d3
pierreyoda <pierreyoda@users.noreply.github.com>
2022-10-08 10:03:27 +0200

    :bug: Fix static build

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,9 @@ import type { LayoutLoad } from "./$types";
 
 import type { NavigationSelectableCategory } from "$lib/types";
 
+export const csr = false;
+export const prerender = true;
+
 export const load: LayoutLoad = async ({ url }) => ({
   selectedCategory: ((): NavigationSelectableCategory =>
     url.pathname === "/blog/about" ? "about" : url.pathname.startsWith("/blog") ? "blog" : "projects")(),
