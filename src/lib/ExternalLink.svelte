801c0090fee34ad62e60dc3e7407debb78ca3982
pierreyoda <pierreyoda@users.noreply.github.com>
2022-12-08 18:42:17 +0100

    :sparkles: Add GitHub link to this website's repository in NavBar

---
+++ src/lib/ExternalLink.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let href: string;
+  export let label: string;
+  export let inNewTab = true;
+
+  $: target = inNewTab ? "_blank" : "_self";
+</script>
+
+<a {href} {target} rel="noopener noreferrer">
+  {label}
+</a>
+
+<style lang="postcss">
+  a {
+    @apply no-underline text-white font-semibold;
+  }
+</style>
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,26 +1,36 @@
 <script lang="ts">
   import NavbarLink from "./NavbarLink.svelte";
+  import ExternalLink from "./ExternalLink.svelte";
   import type { NavigationSelectableCategory } from "./types";
 
   export let selectedCategory: NavigationSelectableCategory;
 </script>
 
-<div class="navbar-container">
-  <div class="flex items-center">
-    <a data-sveltekit-prefetch href="/">
-      <img class="w-32 h-auto" src="/brand.svg" alt="Praca logo" />
-    </a>
+<div class="container">
+  <div class="navbar-container">
+    <div class="flex items-center">
+      <a data-sveltekit-prefetch href="/">
+        <img class="w-28 h-auto" src="/brand.svg" alt="Praca logo" />
+      </a>
+    </div>
+    <nav>
+      <ul class="flex items-center flex-grow md:text-xl">
+        <NavbarLink href="/" label="Projects" isSelected={selectedCategory === "projects"} />
+        <NavbarLink href="/blog" label="Blog" isSelected={selectedCategory === "blog"} />
+        <NavbarLink href="/blog/about" label="About" isSelected={selectedCategory === "about"} />
+      </ul>
+    </nav>
   </div>
-  <nav>
-    <ul class="flex items-center flex-grow md:text-xl">
-      <NavbarLink href="/" label="Projects" isSelected={selectedCategory === "projects"} />
-      <NavbarLink href="/blog" label="Blog" isSelected={selectedCategory === "blog"} />
-      <NavbarLink href="/blog/about" label="About" isSelected={selectedCategory === "about"} />
-    </ul>
-  </nav>
+  <div class="github-link-container">
+    <ExternalLink href="https://github.com/pierreyoda/praca-website" label="GitHub" />
+  </div>
 </div>
 
 <style lang="postcss">
+  .container {
+    @apply w-full flex flex-col self-center items-center;
+  }
+
   .navbar-container {
     @apply flex items-center w-full p-2;
     @apply max-w-3xl mx-auto;
@@ -31,7 +41,14 @@
     }
   }
 
+  .github-link-container {
+  }
+
   @screen md {
+    .container {
+      @apply flex-row;
+    }
+
     .navbar-container {
       nav {
         @apply text-xl;
