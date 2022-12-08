2902b1f21b0bcbde7e6ad4845ed751d7f3e9afd0
pierreyoda <pierreyoda@users.noreply.github.com>
2022-12-08 09:20:56 +0100

    :sparkles: Custom Vite plugin to display images with Remark and Mdsvex

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -8,9 +8,8 @@ const dev = mode === "development";
 
 const config = {
   plugins: [
-    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
     tailwindcss(),
-    //But others, like autoprefixer, need to run after,
+    // autoprefixer needs to be setup after tailwindcss
     autoprefixer(),
     nested(),
     !dev &&
