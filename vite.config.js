cba03dff0c230fbb1a1aa50b841c7ba76d024196
pierreyoda <pierreyoda@users.noreply.github.com>
2022-12-09 19:39:38 +0100

    :arrow_up: Upgrade Vite to v4 (which means rollup 3)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
-import { mdsvex } from "mdsvex";
-import preprocess from "svelte-preprocess";
-import remarkEmoji from "remark-emoji";
 import { sveltekit } from "@sveltejs/kit/vite";
-import staticAdapter from "@sveltejs/adapter-static";
 
 /** @type {import("vite").UserConfig} */
 const config = {
