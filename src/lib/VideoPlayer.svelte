7144e0dcd726efb6588d458c6678d564414dc616
pierreyoda <pierreyoda@users.noreply.github.com>
2022-12-13 15:52:05 +0100

    :iphone: Improve VideoPlayer article component responsiveness

--- src/lib/VideoPlayer.svelte
+++ src/lib/VideoPlayer.svelte
@@ -4,4 +4,10 @@
   export let title: string;
 </script>
 
-<video {src} {title} playsinline autoplay muted loop />
+<video {src} {title} playsinline autoplay muted loop class="video-player" />
+
+<style lang="postcss">
+  .video-player {
+    @apply w-full max-h-full;
+  }
+</style>
