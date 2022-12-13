fd9914ec90388f77ca6d3bb37c9abd523a66c571
pierreyoda <pierreyoda@users.noreply.github.com>
2022-12-13 15:53:51 +0100

    :sparkles: Improve Navbar, quickly align with new naming (newstackwhodis)

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -4,37 +4,47 @@
   import type { NavigationSelectableCategory } from "./types";
 
   export let selectedCategory: NavigationSelectableCategory;
+
+  $: hideGitHubLink = selectedCategory === "blog";
+  $: githubLinkContainerClasses = hideGitHubLink ? "github-link-container hidden" : "github-link-container";
 </script>
 
-<div class="container">
-  <div class="navbar-container">
-    <div class="flex items-center">
-      <a data-sveltekit-prefetch href="/">
-        <img class="w-28 h-auto" src="/brand.svg" alt="Praca logo" />
-      </a>
-    </div>
-    <nav>
-      <ul class="flex items-center flex-grow md:text-xl">
-        <NavbarLink href="/" label="Projects" isSelected={selectedCategory === "projects"} />
-        <NavbarLink href="/blog" label="Blog" isSelected={selectedCategory === "blog"} />
-        <NavbarLink href="/blog/about" label="About" isSelected={selectedCategory === "about"} />
-      </ul>
-    </nav>
+<div class="outer-container">
+  <div class="w-full justify-center flex items-center mr-12 text-white font-bold py-6">
+    <a data-sveltekit-prefetch href="/">
+      newstackwhodis
+    </a>
   </div>
-  <div class="github-link-container">
-    <ExternalLink href="https://github.com/pierreyoda/praca-website">
-      GitHub
-    </ExternalLink>
+  <div class="container">
+    <div />
+    <div class="navbar-container">
+      <nav>
+        <ul class="flex items-center flex-grow md:text-xl">
+          <NavbarLink href="/" label="Projects" isSelected={selectedCategory === "projects"} />
+          <NavbarLink href="/blog" label="Blog" isSelected={selectedCategory === "blog"} />
+          <NavbarLink href="/blog/about" label="About" isSelected={selectedCategory === "about"} />
+        </ul>
+      </nav>
+    </div>
+    <div class={githubLinkContainerClasses}>
+      <ExternalLink href="https://github.com/pierreyoda/praca-website">
+        GitHub
+      </ExternalLink>
+    </div>
   </div>
 </div>
 
 <style lang="postcss">
+  .outer-container {
+    @apply flex flex-col;
+  }
+
   .container {
-    @apply w-full flex flex-col self-center items-center;
+    @apply w-full flex flex-col self-center items-center justify-between;
   }
 
   .navbar-container {
-    @apply flex items-center w-full p-2;
+    @apply flex items-center p-2;
     @apply max-w-3xl mx-auto;
     @apply text-gray-lighter;
 
@@ -44,7 +54,7 @@
   }
 
   .github-link-container {
-    @apply text-white font-semibold;
+    @apply text-white font-semibold py-2;
   }
 
   @screen md {
@@ -56,6 +66,10 @@
       nav {
         @apply text-xl;
       }
+    }
+
+    .github-link-container {
+      @apply py-0;
     }
   }
 </style>
