635c950807ffe96a712f4e3fc475aefaf850f9af
pierreyoda <pierreyoda@users.noreply.github.com>
2025-06-26 20:10:13 +0200

    :lipstick: Improved Navbar design

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -12,7 +12,7 @@ interface NavbarLinkProps {
 }
 
 const NavbarLink: FunctionComponent<NavbarLinkProps> = ({ href, label, selected }) => (
-  <li className="mr-8 last:mr-0">
+  <li className="mr-8">
     <Link href={href} className={clsx("hover:text-lychee cursor-pointer", selected ? "text-lychee" : "text-white")}>
       {label}
     </Link>
@@ -31,8 +31,7 @@ export const Navbar: FunctionComponent<NavbarProps> = ({ category }) => (
     <div className="mr-12 flex w-full items-center justify-center py-6 font-bold">
       <Link href="/">newstackwhodis</Link>
     </div>
-    <div className="flex w-full flex-col items-center justify-between self-center md:flex-row">
-      <div />
+    <div className="mx-auto flex w-full items-center justify-between">
       <header className="text-gray-lighter mx-auto flex max-w-3xl items-center p-2 md:text-xl">
         <nav className="flex grow items-center">
           <ul className="flex grow items-center md:text-xl">
@@ -41,19 +40,23 @@ export const Navbar: FunctionComponent<NavbarProps> = ({ category }) => (
             <NavbarLink href="/blog/about" label="About" selected={category === "about"} />
           </ul>
         </nav>
+        <div className="flex items-center font-semibold">
+          <div className="mr-4">
+            <ExternalLink href={HNCLI_WEBSITE_URL} className="text-light-orange/70 hover:text-light-orange">
+              hncli
+            </ExternalLink>
+          </div>
+          {/* <div className={clsx("py-2 text-center md:py-0", category === "blog" && "hidden")}> */}
+          <div className="mr-4">
+            <ExternalLink
+              href="https://github.com/pierreyoda/"
+              className="text-light-orange/70 hover:text-light-orange"
+            >
+              GitHub
+            </ExternalLink>
+          </div>
+        </div>
       </header>
-      <div className="flex items-center font-semibold">
-        <div className="mr-4">
-          <ExternalLink href={HNCLI_WEBSITE_URL} className="text-light-orange/70 hover:text-light-orange">
-            hncli
-          </ExternalLink>
-        </div>
-        <div className={clsx("text-centerxx py-2 md:py-0", category === "blog" && "hidden")}>
-          <ExternalLink href="https://github.comp/ierreyoda/" className="text-light-orange/70 hover:text-light-orange">
-            GitHub
-          </ExternalLink>
-        </div>
-      </div>
     </div>
   </div>
 );
