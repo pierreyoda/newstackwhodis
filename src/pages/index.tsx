35422f5fba06734e9cdabcadef9d607183b0d13d
pierreyoda <pierreyoda@users.noreply.github.com>
2025-06-27 11:49:51 +0200

    :lipstick: Small tweak of home (projects) page‹

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const { githubRepositories } = parsedRepositoriesData();
 // TODO: highlighted projects by selected tag
 const Home: NextPage = () => (
   <div className="flex flex-col">
-    <h1 className="mb-4 text-4xl font-bold md:mb-12">Personal Projects</h1>
+    <h1 className="mp-12 mb-6 text-4xl font-bold">Personal Projects</h1>
     <ProjectsPanel content={githubRepositories} />
   </div>
 );
